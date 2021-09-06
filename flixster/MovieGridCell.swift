231fa32629ddebbecb456d52827c7bc10666f3f1
hong <hongwootan@outlook.com>
2021-09-05 22:11:33 -0400

    adding superhero section

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Hong Woo on 9/5/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
